83e28767fe51882798826c53e8e7acaab14b4041
taoganll <3201094591@qq.com>
2018-03-04 23:43:29 +0800

    一半首页功能

--- public/pc/js/common.js
+++ public/pc/js/common.js
@@ -1,3 +1,27 @@
 /**
  * Created by MyPC on 2018/3/3.
  */
+$(function () {
+    NProgress.configure({
+        showSpinner: false
+    })
+
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    })
+    $(document).ajaxStop(function () {
+        setTimeout(function () {
+            NProgress.done();
+        }, 500)
+    })
+
+
+    $('.nav li').on('click', function () {
+         $('.er-nav').slideToggle(500);
+
+    })
+    $('.disappear').on('click',function(){
+        $('.it_aside').toggleClass('now')
+        $('.loginOut').toggleClass('now')
+    })
+})

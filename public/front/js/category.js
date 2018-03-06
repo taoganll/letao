81b4c4c943866d58ab9190486c5cba2c3ee8729a
taoganll <3201094591@qq.com>
2018-03-06 17:06:00 +0800

    移动端分类管理

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -1,3 +1,32 @@
 /**
  * Created by MyPC on 2018/3/6.
  */
+$(function () {
+    $.ajax({
+        type:'get',
+        url:'/category/queryTopCategory',
+        success:function(info){
+            //console.log(info);
+            $('.lt-ul1').html(template("tmp",info))
+            renderScrent(info.rows[0].id)
+        }
+    })
+    function renderScrent(id){
+        $.ajax({
+            type:'get',
+            url:'/category/querySecondCategory',
+            data:{id:id},
+            success:function(info){
+                //console.log(info)
+                $('.lt-ul2').html(template("tmp-ul",info));
+            }
+        })
+    }
+    $('.lt-ul1').on('click','li',function(){
+       $(this).addClass("now").siblings().removeClass("now")
+        var id = $(this).data("id");
+        renderScrent(id);
+        mui('.mui-scroll-wrapper').scroll()[1].scrollTo(0,0,100);//100ºÁÃë¹ö¶¯µ½¶¥
+        //console.log(id)
+    })
+})

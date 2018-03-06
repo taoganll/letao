1b6d413d9881980f9b89bf9f9cf82311daf77f4b
taoganll <3201094591@qq.com>
2018-03-06 11:24:42 +0800

    修改分页

--- public/pc/js/user.js
+++ public/pc/js/user.js
@@ -2,28 +2,28 @@
  * Created by MyPC on 2018/3/4.
  */
 $(function () {
-    var page = 1;
-    var pageSize = 5;
-
-    function render() {
+    var currentPage = 1;
+    var  pageSize = 5;
+//分院与渲染
+     function render() {
         $.ajax({
             type: 'GET',
             url: '/user/queryUser',
-            data:{
-                page:page,
-                pageSize:pageSize
+            data: {
+                page:currentPage ,
+                pageSize: pageSize
             },
-            success:function (info){
-              console.log(info);
-                $('tbody').append(template('tmp',info))
+            success: function (info) {
+                console.log(info);
+                $('tbody').html(template('tmp', info))
 
                 $("#paginator").bootstrapPaginator({
-                    bootstrapMajorVersion:3, //如果使用了bootstrap3版本，必须指定
-                    currentPage: page,  //设置当前页
-                    totalPages: Math.ceil(info.total/info.size),//设置总页数
-                    numberOfPages:5,// 设置显示多少页
+                    bootstrapMajorVersion: 3, //如果使用了bootstrap3版本，必须指定
+                    currentPage: currentPage,  //设置当前页
+                    totalPages: Math.ceil(info.total / pageSize),//设置总页数
+                    //numberOfPages: 5,// 设置显示多少页
                     //当页码被点击的时候触发
-                    onPageClicked: function (a,b,c,p) {
+                    onPageClicked: function (a, b, c, p) {
                         //修改一下page的值
                         page = p;
                         //重新渲染
@@ -31,9 +31,13 @@ $(function () {
                     }
 
                 });
+
             }
         })
     }
+
     render();
+    
+
 })
 

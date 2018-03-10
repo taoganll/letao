75fc8463f4ce0337372354c479dbda00b7c2282b
taoganll <3201094591@qq.com>
2018-03-10 21:54:29 +0800

    商品管理页面渲染和分页完成

--- public/pc/js/classification.js
+++ public/pc/js/classification.js
@@ -1,3 +1,84 @@
 /**
  * Created by MyPC on 2018/3/4.
  */
+$(function(){
+    var page = 1;
+    var pageSize = 2;
+    $('.btn-add').on('click', function () {
+        $('#Add-to').modal('show');
+        $.ajax({
+            type: 'get',
+            url: '/category/querySecondCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            success: function (info) {
+                $('.dropdown-menu').html(template('tmp-l', info))
+            }
+        })
+    })
+    //��Ⱦ��ҳ������
+    function render(){
+        $.ajax({
+            type:'get',
+            url:'/product/queryProductDetailList',
+            data:{
+                page:page,
+                pageSize:pageSize
+            },
+            success:function(info){
+                //console.log(info)
+                $('.field').html(template('tmp',info));
+                $("#paginator").bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: page,
+                    totalPages: Math.ceil(info.total / info.size),
+                    size:"normal",
+
+                    //itemTexts����ص��������ÿһ����ť��ִ��һ�Σ�����ֵ����ÿһ����ť��ʾ������
+                    //type:ָÿ����ť������   ��ҳ-first  ��һҳ-prev  ҳ��-page  ��һҳ-next ���һ��-last
+                    //page:ָ����ÿ����ť��Ӧ��ҳ��ֵ
+                    itemTexts: function (type, page, current) {
+
+
+                        //console.log(type, page, current);
+                        switch (type) {
+                            case "first":
+                                return "首页";
+                            case "prev":
+                                return "上一页";
+                            case "next":
+                                return "下一页";
+                            case "last":
+                                return "尾页";
+                            default:
+                                return "第"+page+"页";
+                        }
+                    },
+                    tooltipTitles: function (type, page, current) {
+                        //console.log(type, page, current);
+                        switch (type) {
+                            case "first":
+                                return "首页";
+                            case "prev":
+                                return "上一页";
+                            case "next":
+                                return "下一页";
+                            case "last":
+                                return "尾页";
+                            default:
+                                return "第"+page+"页";
+                        }
+                    },
+                    useBootstrapTooltip:true,
+                    onPageClicked: function (a, b, c, p) {
+                        page = p;
+                        render();
+                    }
+                });
+            }
+        })
+    }
+    render();
+})
